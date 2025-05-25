c650a89fab944494223468664532754bca3772c5
pjsmith404 <pjsmith404@gmail.com>
2025-05-25 21:14:35 +1000

    rearrange classes

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ class ControlFrame(ttk.Frame):
         self.okay_button = ttk.Button(self, text="Add Plant", command=self._map.add_plant)
         self.okay_button.pack()
 
+class MapFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.pack(side="right")
+        self._canvas = tk.Canvas(self, bg="white", width=300, height=300)
+        self._canvas.pack()
+        self._plants = []
+
+    def add_plant(self):
+        self._plants.append(Plant("Test", "2025-01-01", self._canvas))
+
 class Plant:
     def __init__(self, name, planted, canvas):
         self.name = name
@@ -31,16 +42,4 @@ class Plant:
             fill="green",
             width=2
         )
-
-class MapFrame(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.pack(side="right")
-        self._canvas = tk.Canvas(self, bg="white", width=300, height=300)
-        self._canvas.pack()
-        self._plants = []
-
-    def add_plant(self):
-        self._plants.append(Plant("Test", "2025-01-01", self._canvas))
-
 
