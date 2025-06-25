19fccd379f136b988faf57676b3b40a072d082d8
pjsmith404 <pjsmith404@gmail.com>
2025-06-25 20:02:48 +1000

    Add some map canvas tests

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import tkinter as tk
 
-from app import App,AppMenu
+from app import App, AppMenu, MapCanvas
 
 class TestApp(unittest.TestCase):
     def test_app_config(self):
@@ -81,6 +81,33 @@ class TestAppMenu(unittest.TestCase):
         self.assertEqual(self.menubar.menu_file.entrycget("Save As...", "state"), "disabled")
         self.assertEqual(self.menubar.menu_file.entrycget("Close", "state"), "disabled")
         self.assertEqual(self.menubar.menu_plants.entrycget("Add Plant", "state"), "disabled")
+
+class TestMapCanvas(unittest.TestCase):
+    def test_config(self):
+        root = tk.Tk()
+        canvas = MapCanvas(root)
+
+        self.assertEqual(canvas._background, None)
+        self.assertEqual(canvas._state, {})
+
+    def test_add_plant(self):
+        root = tk.Tk()
+        canvas = MapCanvas(root)
+
+        test_state = {
+            1: {
+                "name": "1",
+                "planted": "1",
+                "x1": 10.0,
+                "x2": 20.0,
+                "y1": 10.0,
+                "y2": 20.0
+                }
+            }
+
+        canvas.add_plant(name="1", planted="1")
+
+        self.assertEqual(canvas.get_canvas_state(), test_state)
 
 if __name__ == "__main__":
     unittest.main()
